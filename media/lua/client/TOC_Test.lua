13010b7b2a39cd9e210a5b461ed51257c4e2c704
Pao <picchetti1998@gmail.com>
2023-01-18 01:33:06 +0100

    Changed all the names

--- media/lua/client/TOC_Test.lua
+++ media/lua/client/TOC_Test.lua
@@ -35,73 +35,96 @@ function TestStuffToc()
 
     local sides = {"Left", "Right"}
     local limbs = {"Hand", "LowerArm", "UpperArm"}          -- Let's follow their naming
+
+
+    local prosthesis_list = {"WoodenHook", "MetalHook", "MetalHand"}
+
+
+
+    local accepted_prosthesis_hand = {"WoodenHook", "MetalHook", "MetalHand"}
+    local accepted_prosthesis_lowerarm = {"WoodenHook", "MetalHook", "MetalHand"}
+    local accepted_prosthesis_upperarm = {}     -- For future stuff
+
+
+
+
+
+    for _, side in ipairs(sides) do
+        for _, limb in ipairs(limbs) do
+
+            local part_name = side .. "_" .. limb
+
+            mod_data.TOC.Limbs[part_name].is_cut = false
+            mod_data.TOC.Limbs[part_name].is_infected = false
+            mod_data.TOC.Limbs[part_name].is_operated = false
+            mod_data.TOC.Limbs[part_name].is_cicatrized = false
+            mod_data.TOC.Limbs[part_name].is_cauterized = false
+            mod_data.TOC.Limbs[part_name].is_amputation_shown = false
     
+            mod_data.TOC.Limbs[part_name].cicatrization_time = 0
+            
+            
+            mod_data.TOC.Limbs[part_name].is_prosthesis_equipped = false
+            mod_data.TOC.Limbs[part_name].prosthesis_factor = 1.0
+            mod_data.TOC.Limbs[part_name].prosthesis_material_id = nil
+
+            
+            -- Even if there are some duplicates, this is just easier in the end since we're gonna get fairly easily part_name
+            if limb == "Hand" then
+                mod_data.TOC.Prosthesis.AcceptedProsthesis[part_name] = accepted_prosthesis_hand
 
 
 
+            elseif limb == "LowerArm" then
+                mod_data.TOC.Prosthesis.AcceptedProsthesis[part_name] = accepted_prosthesis_lowerarm
+            elseif limb == "UpperArm" then
+                mod_data.TOC.Prosthesis.AcceptedProsthesis[part_name] = accepted_prosthesis_upperarm
+            end
+  
+        end
+    end
 
-
-
-
-
-
-
-    -- Setup prosthesis table
-
-    local prosthesis_table = {
-    }
-
-
-
-
-
-    for _ ,v in pairs(GetBodyParts()) do
-        mod_data.TOC.Limbs[v].is_cut = false
-        mod_data.TOC.Limbs[v].is_infected = false
-        mod_data.TOC.Limbs[v].is_operated = false
-        mod_data.TOC.Limbs[v].is_cicatrized = false
-        mod_data.TOC.Limbs[v].is_cauterized = false
-        mod_data.TOC.Limbs[v].is_amputation_shown = false
-
-        mod_data.TOC.Limbs[v].cicatrization_time = 0
+    for _, v in ipairs(prosthesis_list) do
         
-        
-        mod_data.TOC.Limbs[v].is_prosthesis_equipped = false
-        mod_data.TOC.Limbs[v].prosthesis_factor = 1.0
-        mod_data.TOC.Limbs[v].prosthesis_material_id = nil
+        mod_data.TOC.Prosthesis[v].prosthesis_factor = 1.0      -- Default
+        mod_data.TOC.Limbs[v].prosthesis_material_id = nil      -- Set texture?
 
-
-        -- Prosthesis part
-        mod_data.TOC.Prosthesis[v].accepted_prosthesis = {}
-        mod_data.TOC.Prosthesis
-
-
+        -- TODO Something else?
 
     end
 
-
-    mod_data.TOC.Prosthesis.list = GetProsthesisList()
-
-    for _,v in ipairs(GetProsthesisLisHumanReadable()) do   
-        mod_data.TOC.Prosthesis[v].
-        
+end
 
 
+function Test2Toc(part_name, prosthetic_name)
+
+    --------
+    -- NEW NAMING SCHEME
+
+    ---- Amputations
+
+    -- Amputation_Left_Hand
+    -- Amputation_Right_UpperArm
+
+
+    ---- Prosthesis to equip
+    -- Prost_Left_Hand_MetalHook
+    -- Prost_Right_Forearm_WoodenHook
+
+    --- Objects
+    -- Prost_Object_WoddenHook
+
+
+    -- First value side, second value limb
+    local part_name_table = {}
+
+    for v in part_name:gmatch("([^_]+)") do
+        table.insert(part_name_table, v)
     end
 
-
-
-
-        RightHand = {},
-        RightForearm = {},
-        RightArm = {},
-
-        LeftHand = {},
-        LeftForearm = {},
-        LeftArm = {},
-
-        is_other_bodypart_infected = false
-    }
+    -- TODO Probably add TOC. before prost_
+    local prost_to_equip_name = "Prost_" .. part_name_table[1] .. "_" .. part_name_table[2] .. "_" .. prosthetic_name
+    return prost_to_equip_name
 
 
 end
