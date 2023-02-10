e84b6b65f1f234ecf2aa4fc4b8e1f243a5e98294
Pao <picchetti1998@gmail.com>
2023-02-11 00:15:41 +0100

    Added malus after amputation in traits

--- media/lua/client/TOC_Init.lua
+++ media/lua/client/TOC_Init.lua
@@ -261,14 +261,20 @@ local function TocDeclareTraits()
     local amp1 = TraitFactory.addTrait("Amputee_Hand", getText("UI_trait_Amputee_Hand"), -8,
         getText("UI_trait_Amputee_Hand_desc"), false, false)
     amp1:addXPBoost(Perks.Left_Hand, 4)
+    amp1:addXPBoost(Perks.Fitness, -1)
+    amp1:addXPBoost(Perks.Strength, -1)
 
     local amp2 = TraitFactory.addTrait("Amputee_LowerArm", getText("UI_trait_Amputee_LowerArm"), -10,
         getText("UI_trait_Amputee_LowerArm_desc"), false, false)
     amp2:addXPBoost(Perks.Left_Hand, 4)
+    amp2:addXPBoost(Perks.Fitness, -1)
+    amp2:addXPBoost(Perks.Strength, -1)
 
     local amp3 = TraitFactory.addTrait("Amputee_UpperArm", getText("UI_trait_Amputee_UpperArm"), -20,
         getText("UI_trait_Amputee_UpperArm_desc"), false, false)
     amp3:addXPBoost(Perks.Left_Hand, 4)
+    amp3:addXPBoost(Perks.Fitness, -1)
+    amp3:addXPBoost(Perks.Strength, -1)
 
     TraitFactory.addTrait("Insensitive", getText("UI_trait_Insensitive"), 6, getText("UI_trait_Insensitivedesc"), false,
         false)
