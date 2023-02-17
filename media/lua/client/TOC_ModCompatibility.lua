393e04f9466c65412783f39de952abe01c986c6a
Pao <picchetti1998@gmail.com>
2023-02-17 12:12:05 +0100

    Working SwapIt + FancyHandwork compat

--- media/lua/client/TOC_ModCompatibility.lua
+++ media/lua/client/TOC_ModCompatibility.lua
@@ -4,20 +4,15 @@
 
 TOC_ModTable = {
     FancyHandwork = false,
+    SwapIt = false,
     LeftIsRight = false,
 }
 
 
-
 local function SetCompatibilityFancyHandwork()
-    if getActivatedMods():contains('FancyHandwork') == false then return end
-    require "TimedActions/FHSwapHandsAction"
-
-
     local og_ISHotbar_equipItem = ISHotbar.equipItem
 
     function ISHotbar:equipItem(item)
-        print("TOC: Overriding FancyHandwork methods")
         local mod = isFHModKeyDown()
         local primary = self.chr:getPrimaryHandItem()
         local secondary = self.chr:getSecondaryHandItem()
@@ -29,11 +24,6 @@ local function SetCompatibilityFancyHandwork()
         -- TODO not totally realiable
         TocPopulateCanBeHeldTable(can_be_held, limbs_data)
 
-
-        -- for _, test in pairs(can_be_held) do
-        --     print(test)
-        -- end
-        --ISInventoryPaneContextMenu.transferIfNeeded(self.chr, item)
 
         -- If we already have the item equipped
         if (primary and primary == item) or (secondary and secondary == item) then
@@ -102,5 +92,124 @@ local function SetCompatibilityLeftIsRight()
     
 end
 
-Events.OnGameStart.Add(SetCompatibilityFancyHandwork)
-Events.OnGameStart.Add(SetCompatibilityLeftIsRight)
+local function SetCompatibilityFancyHandWorkAndSwapIt()
+ -- TODO can't require Swap it for some reason
+
+
+    local og_ISHotbar_equipItem = ISHotbar.equipItem
+
+    function ISHotbar:equipItem(item)
+        local mod = isFHModKeyDown()
+        local primary = self.chr:getPrimaryHandItem()
+        local secondary = self.chr:getSecondaryHandItem()
+        local equip = true
+
+        local limbs_data = getPlayer():getModData().TOC.Limbs
+        local can_be_held = {}
+
+        -- TODO not totally realiable
+        TocPopulateCanBeHeldTable(can_be_held, limbs_data)
+
+
+        -- If we already have the item equipped
+        if (primary and primary == item) or (secondary and secondary == item) then
+            ISTimedActionQueue.add(ISUnequipAction:new(self.chr, item, 20))
+            equip = false
+        end
+
+        -- If we didn't just do something
+        if equip then
+            -- Handle holding big objects
+            if primary and isForceDropHeavyItem(primary) then
+                ISTimedActionQueue.add(ISUnequipAction:new(self.chr, primary, 50))
+                ----- treat "equip" as if we have something equipped from here down
+                equip = false
+            end
+            if mod then
+                --print("TOC: Fancy Handwork modifier")
+                -- If we still have something equipped in secondary, unequip
+                if secondary and equip and can_be_held["Left"] then
+                    ISTimedActionQueue.add(ISUnequipAction:new(self.chr, secondary, 20))
+                end
+
+                if can_be_held["Left"] then
+                    ISTimedActionQueue.add(ISEquipWeaponAction:new(self.chr, item, 20, false, item:isTwoHandWeapon()))
+                elseif can_be_held["Right"] then
+                    ISTimedActionQueue.add(ISEquipWeaponAction:new(self.chr, item, 20, true, item:isTwoHandWeapon()))
+
+                end
+            else
+                -- If we still have something equipped in primary, unequip
+                if primary and equip and can_be_held["Right"] then
+                    ISTimedActionQueue.add(ISUnequipAction:new(self.chr, primary, 20))
+                end
+                -- Equip Primary
+                if can_be_held["Right"] then
+                    ISTimedActionQueue.add(ISEquipWeaponAction:new(self.chr, item, 20, true, item:isTwoHandWeapon()))
+                elseif can_be_held["Left"] then
+                    ISTimedActionQueue.add(ISEquipWeaponAction:new(self.chr, item, 20, false, item:isTwoHandWeapon()))
+
+                end
+            end
+
+            -- Swap It part
+            local i_slotinuse = item:getAttachedSlot()
+            local slot = self.availableSlot[i_slotinuse]
+            local slotIndexID = "swap_Hotbar"..i_slotinuse
+            if slot and SwapItConfig.config[slotIndexID] == true then
+                if primary and not self:isInHotbar(primary) and self:canBeAttached(slot, primary) then
+                    self:removeItem(item, false)--false = don't run animation
+                    self:attachItem(primary, slot.def.attachments[primary:getAttachmentType()], i_slotinuse, slot.def, true)
+                end
+            end
+
+        end
+
+        self.chr:getInventory():setDrawDirty(true)
+        getPlayerData(self.chr:getPlayerNum()).playerInventory:refreshBackpacks()
+    end
+
+    local og_FHSwapHandsAction = FHSwapHandsAction.start
+
+
+    function FHSwapHandsAction:isValid()
+        local limbs_data = getPlayer():getModData().TOC.Limbs
+        local can_be_held = {}
+        TocPopulateCanBeHeldTable(can_be_held, limbs_data)
+        return  (can_be_held["Right"] and can_be_held["Left"]) and(((self.character:getPrimaryHandItem() or self.character:getSecondaryHandItem()) ~= nil))
+    end
+end
+
+--------------------------------------------------------
+
+function TOC_CheckModCompatibility()
+    local activated_mods = getActivatedMods()
+    print("TOC: Checking mods")
+
+
+    if activated_mods:contains("FancyHandwork") then
+
+        if activated_mods:contains("SwapIt") then
+            require "SwapIt Main"
+            print("TOC: Overriding FancyHandwork and SwapIt methods")
+            SetCompatibilityFancyHandWorkAndSwapIt()
+        else
+            print("TOC: Overriding FancyHandwork methods")
+            require "TimedActions/FHSwapHandsAction"
+            SetCompatibilityFancyHandwork()
+        end
+    end
+
+    if activated_mods:contains("LeftIsRight") then
+        TOC_ModTable.LeftIsRight = true
+    end
+
+
+
+
+end
+
+print("TOC: Starting CheckModCompatibility")
+
+Events.OnGameStart.Add(TOC_CheckModCompatibility)
+
