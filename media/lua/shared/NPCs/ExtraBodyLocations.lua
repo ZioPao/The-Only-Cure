b195c92466d567bb944a3daed4721664c92f5e14
Pao <picchetti1998@gmail.com>
2023-02-26 22:16:45 +0100

    Modified Mod.info and debug prints

--- media/lua/shared/NPCs/ExtraBodyLocations.lua
+++ media/lua/shared/NPCs/ExtraBodyLocations.lua
@@ -3,7 +3,7 @@ local function AddBodyLocationBefore(new_location, move_to_location)
   local list = getClassFieldVal(group, getClassField(group, 1))
   group:getOrCreateLocation(new_location)
   local new_item = list:get(list:size()-1)
-  print("TOC: Created new body location" .. new_item:getId())
+  print("JCIO: Created new body location" .. new_item:getId())
   list:remove(new_item)   -- We can't use the Index, it works if we pass the item though!
   local i = group:indexOf(move_to_location)
   list:add(i, new_item)
