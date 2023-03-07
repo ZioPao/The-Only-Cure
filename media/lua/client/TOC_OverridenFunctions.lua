98dca9781d838af313373d362ec4e03df6896a5b
Pao <picchetti1998@gmail.com>
2023-03-07 18:50:28 +0100

    Added hotfix to loot all

--- media/lua/client/TOC_OverridenFunctions.lua
+++ media/lua/client/TOC_OverridenFunctions.lua
@@ -188,3 +188,23 @@ function ISInventoryPaneContextMenu.dropItem(item, player)
     end
 
 end
+
+
+local og_ISInventoryPagePrerender = ISInventoryPage.prerender
+
+function ISInventoryPage:prerender()
+    -- Check if there is any amputated limb here. if there is, just fail and maybe notify the player
+    og_ISInventoryPagePrerender(self)
+    if TocCheckIfAnyAmputationItemInInventory(self.inventory) then
+        self.canLootAll = false
+    else
+        self.canLootAll = true
+    end
+end
+
+local og_ISInventoryPageLootAll = ISInventoryPage.lootAll
+function ISInventoryPage:lootAll()
+    if self.canLootAll then
+        og_ISInventoryPageLootAll(self)
+    end
+end
