b6fb136098286aa2b74a9c643a49ca76ddde252a
ZioPao <picchetti1998@gmail.com>
2024-03-24 23:45:35 +0100

    Compat for FH

--- media/lua/client/TOC/Compat.lua
+++ media/lua/client/TOC/Compat.lua
@@ -1,14 +1,38 @@
 
---* BRUTAL HANDS -- *
 
 local function HandleModCompatibility()
 
-    if getActivatedMods():contains('BrutalHandwork') then
+    --* BRUTAL HANDS -- *
+    --[[
+        Brutal hands has a TOC_COMPAT but its check is wrong and uses an old API.
+    ]]
+
+    local activatedMods = getActivatedMods()
+    TOC_DEBUG.print("Checking for mods compatibility")
+
+    if activatedMods:contains('BrutalHandwork') then
+        TOC_DEBUG.print("found BrutalHandwork, activating compat module")
         BrutalHands = BrutalHands or {}
         BrutalHands.TOC = require("TOC/API")
-
-        -- Brutal hands has a TOC_COMPAT but its check is wrong and uses an old API.
     end
+    if activatedMods:contains('FancyHandwork') then
+        TOC_DEBUG.print("found FancyHandwork, activating compat module")
+
+        require("TimedActions/FHSwapHandsAction")
+        local og_FHSwapHandsAction_isValid = FHSwapHandsAction.isValid
+        function FHSwapHandsAction:isValid()
+            local tocApi = require("TOC/API")
+            if tocApi.hasBothHands(self.character) then
+                return og_FHSwapHandsAction_isValid(self)
+            else
+                return false
+            end
+        end
+
+
+
+    end
+
 end
 
 Events.OnGameStart.Add(HandleModCompatibility)
