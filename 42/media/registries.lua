acd2e9c547d159fe89fdac2727af30330954956d
guiriguy <67844553+guiriguy@users.noreply.github.com>
2025-12-18 22:46:57 +0100

    Refactor TOC registries for traits and body locations

--- 42/media/registries.lua
+++ 42/media/registries.lua
@@ -1,15 +1,16 @@
-TOC_Registries = {
-    Traits = {
-        Amputee_Hand = CharacterTrait.register("toc:amputee_hand"),
-        Amputee_Forearm = CharacterTrait.register("toc:amputee_forearm"),
-        Amputee_Upperarm = CharacterTrait.register("toc:amputee_upperarm")
-    }
+_TOCRegistries = {}
+
+_TOCRegistries.traits = {
+    Amputee_Hand = CharacterTrait.register("toc:amputee_hand"),
+    Amputee_ForeArm = CharacterTrait.register("toc:amputee_forearm"),
+    Amputee_UpperArm = CharacterTrait.register("toc:amputee_upperarm"),
 }
 
-
-ItemBodyLocation.register("toc:TOC_Arm_L")
-ItemBodyLocation.register("toc:TOC_Arm_R")
-ItemBodyLocation.register("toc:TOC_ArmProst_L")
-ItemBodyLocation.register("toc:TOC_ArmProst_R")
-ItemBodyLocation.register("toc:TOC_ArmAccessory_L")
-ItemBodyLocation.register("toc:TOC_ArmAccessory_R")
+_TOCRegistries.bodylocations = {
+    TOC_Arm_L = ItemBodyLocation.register("toc:toc_arm_l"),
+    TOC_Arm_R = ItemBodyLocation.register("toc:toc_arm_r"),
+    TOC_ArmProst_L = ItemBodyLocation.register("toc:toc_armprost_l"),
+    TOC_ArmProst_R = ItemBodyLocation.register("toc:toc_armprost_r"),
+    TOC_ArmAccessory_L = ItemBodyLocation.register("toc:toc_armaccessory_l"),
+    TOC_ArmAccessory_R = ItemBodyLocation.register("toc:toc_armaccessory_r"),
+}
