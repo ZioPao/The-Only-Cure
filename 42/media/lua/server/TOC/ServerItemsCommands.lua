e45dfcdea9723a5a2a98505c7409f7e2a55f63c3
ZioPao <picchetti1998@gmail.com>
2026-02-27 03:15:16 +0100

    fix: sp compat 2

--- 42/media/lua/server/TOC/ServerItemsCommands.lua
+++ 42/media/lua/server/TOC/ServerItemsCommands.lua
@@ -7,29 +7,49 @@ local ItemsController = require("TOC/Controllers/ItemsController")
 local ServerItemsCommands = {}
 
 function ServerItemsCommands.SpawnAmputationItem(_, args)
-    local playerObj = getPlayerByOnlineID(args.playerNum)
+    local patientPl
+    if not isServer() then
+        patientPl = getPlayer()     -- SP
+    else
+        patientPl = getPlayerByOnlineID(args.patientNum)
+    end
     local limbName = args.limbName
-    ItemsController.Player.SpawnAmputationItem(playerObj, limbName)
+    ItemsController.Player.SpawnAmputationItem(patientPl, limbName)
 end
 
 function ServerItemsCommands.DeleteOldAmputationItem(_, args)
-    local patientPl = getPlayerByOnlineID(args.playerNum)
+    local patientPl
+    if not isServer() then
+        patientPl = getPlayer()     -- SP
+    else
+        patientPl = getPlayerByOnlineID(args.patientNum)
+    end
     local limbName = args.limbName
 
     ItemsController.Player.DeleteOldAmputationItem(patientPl, limbName)
 end
 
 function ServerItemsCommands.DeleteAllOldAmputationItems(_, args)
-    local playerObj = getPlayerByOnlineID(args.playerNum)
-    ItemsController.Player.DeleteAllOldAmputationItems(playerObj)
+    local patientPl
+    if not isServer() then
+        patientPl = getPlayer()     -- SP
+    else
+        patientPl = getPlayerByOnlineID(args.patientNum)
+    end
+    ItemsController.Player.DeleteAllOldAmputationItems(patientPl)
 end
 
 function ServerItemsCommands.OverrideAmputationItemVisuals(_, args)
-    local playerObj = getPlayerByOnlineID(args.playerNum)
+    local patientPl
+    if not isServer() then
+        patientPl = getPlayer()     -- SP
+    else
+        patientPl = getPlayerByOnlineID(args.patientNum)
+    end
     local limbName = args.limbName
     local isCicatrized = args.isCicatrized
 
-    ItemsController.Player.OverrideAmputationItemVisuals(playerObj, limbName, isCicatrized)
+    ItemsController.Player.OverrideAmputationItemVisuals(patientPl, limbName, isCicatrized)
 end
 
 
